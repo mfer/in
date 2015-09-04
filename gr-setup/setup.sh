29146ff9d3b4d8da3bf719492fc5c64e48d17488
manassesfereira <sudolshw@gmail.com>
2015-09-04 10:09:01 -0300

    something in the way she moves

---
+++ gr-setup/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#sudo apt-get install cmake libboost-all-dev libgnuradio-runtime python-matplotlib #libzmq1-dev
+
+
+cd /tmp
+wget http://www.sbrac.org/files/build-gnuradio && chmod a+x ./build-gnuradio && ./build-gnuradio
+
+git clone https://github.com/bastibl/gr-foo.git
+cd gr-foo
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+sudo ldconfig
+
+git clone https://github.com/wendley/gr-ieee802-15-4.git
+cd gr-ieee802-15-4/
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+sudo ldconfig
+
+##after connect the device test it with
+#uhd_find_devices
+##to install missing firmwares
+#sudo python "/usr/lib/uhd/utils/uhd_images_downloader.py"
